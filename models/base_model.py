9be65a59b9b967c59c7f692876a322a9a5db89f3
root <lanremidelola@gmail.com>
2023-08-24 02:05:37 +0000

    alx

--- models/base_model.py
+++ models/base_model.py
@@ -2,43 +2,72 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
+import models
+from sqlalchemy import Column, Integer, String, DateTime
+
+Base = declarative_base()
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+    id = Column(String(60), unique=True, nullable=False, primary_key=True)
+    created_at = Column(DateTime,
+                        nullable=False,
+                        default=datetime.utcnow()
+                        )
+    updated_at = Column(DateTime,
+                        default=datetime.utcnow(),
+                        nullable=False
+                        )
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
-        if not kwargs:
-            from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
-        else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+        """Instatntiates a new model
+        Args:
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
+        """
+        self.id = str(uuid4())
+        self.created_at = datetime.utcnow()
+        self.updated_at = self.created_at
+        if kwargs:
+            for key, vle in kwargs.items():
+                if key in ("created_at", "updated_at"):
+                    vle = datetime.strptime(vle, "%Y-%m-%dT%H:%M:%S.%f")
+                if "__class__" not in key:
+                    setattr(self, key, vle)
 
     def __str__(self):
-        """Returns a string representation of the instance"""
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+        """Updates updated_at with current time when instance is changed"""
+        dictt = self.to_dict()
+        cls = str(type(self)).split('.')[-1].split('\'')[0]
+        return "[{:s}] ({:s}) {}".format(cls, self.id,
+                                         dictt)
 
     def save(self):
-        """Updates updated_at with current time when instance is changed"""
-        from models import storage
-        self.updated_at = datetime.now()
-        storage.save()
+        """
+            Updates updated_at with current time when instance is changed
+        """
+        self.updated_at = datetime.utcnow()
+        models.storage.new(self)
+        models.storage.save()
 
     def to_dict(self):
         """Convert instance into dict format"""
-        dictionary = {}
-        dictionary.update(self.__dict__)
-        dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
-        return dictionary
+        dic = {}
+        dic.update(self.__dict__)
+        try:
+            del dic['_sa_instance_state']
+        except Exception:
+            pass
+        dic.update({'__class__':
+                               (str(type(self)).split('.')[-1]).split('\'')[0]})
+        dic['created_at'] = self.created_at.isoformat()
+        dic['updated_at'] = self.updated_at.isoformat()
+
+        return dic
+
+    def delete(self):
+        """
+            delete object
+        """
+        models.storage.delete(self)
--- models/state.py
+++ models/state.py
@@ -1,8 +1,36 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+"""State Module for HBNB project"""
+import models
+from os import getenv
+from models.base_model import Base
 from models.base_model import BaseModel
+from models.city import City
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """State class
+    Attributes:
+        name: input name
+    """
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade='delete', backref="state")
+
+    @property
+    def cities(self):
+        var = models.storage.all()
+        li = []
+        list_city = []
+        for key in var:
+            city = key.replace('.', ' ')
+            city = shlex.split(city)
+            if (city[0] == 'City'):
+                li.append(var[key])
+        for elem in li:
+            if (elem.state_id == self.id):
+                list_city.append(elem)
+        return (list_city)
